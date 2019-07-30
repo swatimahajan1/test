b56352904721d76e8b22b5f04211046bd48a34a2
SamitaMahajan <btl.samita@gmail.com>
2019-07-30 22:06:44 +0530

    interchange 2 no.

---
+++ Swapping.java
@@ -0,0 +1,18 @@
+public class Swapping
+{
+	public static void main(String[] e)
+	{
+	int a=10;
+	int b=20;
+	system.out.println("print a =" +a);
+	system.out.println("print b =" +b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	system.out.println("print after swapping a =" +a);
+	system.out.println("print after swapping b =" +b);
+	}
+}
+
+
+
